49a0a0d743f23f56073a17629b221213133fa4ed
Ezra Bales <bales.ezra@gmail.com>
2025-08-01 15:51:20 -0700

    project 11 edit 2

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,45 +2,27 @@ export class Api {
   constructor() {
     this._baseUrl = "http://localhost:3001";
   }
-  getItems() {
-    return fetch(`${this._baseUrl}/items`)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  _checkResponse(res) {
+    if (!res.ok) {
+      return Promise.reject(new Error(`Fetch error: ${res.status}`));
+    }
+    return res.json();
   }
-  addItem(item) {
-    return fetch(`${this._baseUrl}/items`, {
+  getItems() {
+    return fetch(`${this._baseUrl}/items`).then(this._checkResponse);
+  }
+  async addItem(item) {
+    return await fetch(`${this._baseUrl}/items`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(item),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    }).then(this._checkResponse);
   }
   deleteItem(id) {
     return fetch(`${this._baseUrl}/items/${id}`, {
       method: "DELETE",
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    }).then(this._checkResponse);
   }
 }
