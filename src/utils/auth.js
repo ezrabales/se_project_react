1cff1113caf814228c669083ba7a95dbe01bbdb6
Ezra Bales <bales.ezra@gmail.com>
2025-09-23 17:12:42 -0600

    fix url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrezra.crabdance.com"
+    : "http://localhost:3001";
 
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
